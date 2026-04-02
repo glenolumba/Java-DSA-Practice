bb354cb8d07304125556deaf61843098742e15a0
Glen Olumba <glenolumba257@gmail.com>
2026-04-01 20:42:08 -0400

    Completed outline/pseudocode

--- src/CalculatorProject/Calculator.java
+++ src/CalculatorProject/Calculator.java
@@ -2,10 +2,19 @@ package CalculatorProject;
 
 public class Calculator {
 
-      /*
+    public static void main(String[] args) {
+
+        /*
          Simple calculator
          Declare variables
-
-
+         Get user input
+         Prompt user for first number
+         Prompt user for operand
+         Prompt user for second number
          */
+
+
+
+    }
+
 }
