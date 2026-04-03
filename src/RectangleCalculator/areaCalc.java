1c84940dd593f8baf2ffd35360769f0d7f41f2e2
Glen Olumba <glenolumba257@gmail.com>
2026-04-02 22:10:44 -0400

    Skeleton/Pseudocode for Rectangle Area Calculation

---
+++ src/RectangleCalculator/areaCalc.java
@@ -0,0 +1,18 @@
+package RectangleCalculator;
+
+public class areaCalc {
+
+    public static void main(String[] args){
+
+        /*
+        Area of Rectangle Program
+        Declare Variables
+        Prompt user for length
+        Prompt user for width
+        Get and display area
+         */
+
+
+
+    }
+}
