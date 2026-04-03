96dda98def91337bb62cc875d8b0e811b413d23a
Glen Olumba <glenolumba257@gmail.com>
2026-04-02 22:22:25 -0400

    added basic functionality, user can input length and width

--- src/RectangleCalculator/areaCalc.java
+++ src/RectangleCalculator/areaCalc.java
@@ -1,5 +1,7 @@
 package RectangleCalculator;
 
+import java.util.Scanner;
+
 public class areaCalc {
 
     public static void main(String[] args){
@@ -11,6 +13,26 @@ public class areaCalc {
         Prompt user for width
         Get and display area
          */
+
+        Scanner scanner = new Scanner(System.in);
+
+        double length;
+        double width;
+        double area;
+
+        System.out.println("Calculate your area!");
+
+        System.out.print("Enter your length: ");
+        length = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Enter your width: ");
+        width = scanner.nextDouble();
+        scanner.nextLine();
+
+        area = length * width;
+
+        System.out.printf("Your rectangle's area is %.02f units squared.", area);
 
 
 
