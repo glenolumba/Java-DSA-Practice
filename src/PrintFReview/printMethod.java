1e6b4d85adca1070802da45d6bf64e8062435e32
Glen Olumba <glenolumba257@gmail.com>
2026-04-07 09:29:55 -0400

    basic printf statements with accompanying symbols

--- src/PrintFReview/printMethod.java
+++ src/PrintFReview/printMethod.java
@@ -13,6 +13,11 @@ public class printMethod {
         double amount = 21456.01;
         boolean isRunning = true;
 
+        System.out.printf("The car make is %s.\n", car);
+        System.out.printf("The %s costs %c%.02f.\n", car, symbol, amount);
+        System.out.printf("The %s's year is %d.\n", car, year);
+        System.out.printf("The car is running: %b.\n", isRunning);
+
 
 
     }
