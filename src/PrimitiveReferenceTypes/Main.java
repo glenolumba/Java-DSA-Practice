08f143624bb1eac7227eb4091b83f478044138ee
Glen Olumba <glenolumba257@gmail.com>
2026-04-09 19:21:53 -0400

    intial commit for data type review

---
+++ src/PrimitiveReferenceTypes/Main.java
@@ -0,0 +1,27 @@
+package PrimitiveReferenceTypes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      int age = 30;
+      int year = 2025;
+      int quantity = 1;
+
+      double price = 19;
+      double GPA = 3.5;
+      double temperature = -12.5;
+
+      char grade = 'A';
+      char symbol = '!';
+      char currency = '$';
+
+      boolean isStudent = false;
+      boolean forSale = false;
+      boolean isOnline = true;
+
+      System.out.println(isStudent);
+
+
+    }
+}
