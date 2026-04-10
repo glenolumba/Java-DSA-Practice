e4c86eceeadf0cb38a97413068958a26ae147548
Glen Olumba <glenolumba257@gmail.com>
2026-04-09 20:40:54 -0400

    random practice with int, double, and boolean

--- src/RandomCls/main.java
+++ src/RandomCls/main.java
@@ -8,9 +8,26 @@ public class main {
 
         Random random = new Random();
 
-        int number;
-        number = random.nextInt(1,7);
+        int number1;
+        double number;
+        boolean isHeads;
 
+
+
+        number1 = random.nextInt(1,101);
+        number = random.nextDouble();
+        isHeads = random.nextBoolean();
+
+
+        System.out.println(number1);
         System.out.println(number);
+
+        if(isHeads){
+            System.out.println("HEADS");
+        }
+        else {
+            System.out.println("TAILS");
+        }
+
     }
 }
