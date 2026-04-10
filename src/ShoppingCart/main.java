1786a8f54dc0ecced428a94b6a7de8110af4704c
Glen Olumba <glenolumba257@gmail.com>
2026-04-09 20:17:55 -0400

    initial commit for shopping cart program

---
+++ src/ShoppingCart/main.java
@@ -0,0 +1,20 @@
+package ShoppingCart;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+
+
+    }
+}
