2265fbb1296d3e7ad80a6383261f0950702ab42e
Glen Olumba <glenolumba257@gmail.com>
2026-04-09 20:48:42 -0400

    adding prompts for user

--- src/HypotenuseCalc/main.java
+++ src/HypotenuseCalc/main.java
@@ -1,9 +1,27 @@
 package HypotenuseCalc;
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args){
 
         // Hypotenuse program
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a;
+        double b;
+        double c;
+
+        System.out.print("Enter the length of side A: ");
+        a = scanner.nextDouble();
+
+        System.out.print("Enter the length of side B: ");
+        b = scanner.nextDouble();
+
+
+
+        scanner.close();
     }
 }
