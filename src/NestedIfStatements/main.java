3b503e51e3044ee014165c99e8dbacfca7089f18
Glen Olumba <glenolumba257@gmail.com>
2026-04-20 09:30:19 -0400

    added nested statements within if-else blocks

--- src/NestedIfStatements/main.java
+++ src/NestedIfStatements/main.java
@@ -5,20 +5,29 @@ public class main {
     public static void main(String[] args){
 
         boolean isStudent = true;
-        boolean isSenior;
+        boolean isSenior = true;
         double price = 9.99;
 
-
         if(isStudent){
-            System.out.println("You have a student discount of 10%");
-            price *= 0.9;
+            if(isSenior){
+                System.out.println("You have a senior discount of 20%");
+                System.out.println("You have a student discount of 10%");
+                price *= 0.7;
+            }
+            else {
+                System.out.println("You have a student discount of 10%");
+                price *= 0.9;
+            }
         }
         else {
-            price *= 1;
+            if(isSenior){
+                System.out.println("You have a senior discount of 20%");
+                price *= 0.8;
+            }
+            else {
+                price *= 1; // not necessary
+            }
         }
-
-
         System.out.printf("The ticket price is: $%.02f", price);
-
     }
 }
