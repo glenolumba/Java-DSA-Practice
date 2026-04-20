9fafd6a6159baf7820de4d8002ad2d24723b8cd8
Glen Olumba <glenolumba257@gmail.com>
2026-04-20 09:04:59 -0400

    Added a scanner object with close

--- src/CompoundInterestCalc/main.java
+++ src/CompoundInterestCalc/main.java
@@ -1,10 +1,23 @@
 package CompoundInterestCalc;
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args){
 
         // Compound Interest Program
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+        scanner.close();
 
 
     }
