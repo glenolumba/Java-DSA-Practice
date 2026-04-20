19c732b332528d2f90cc82ef1cf76d72df7bc760
Glen Olumba <glenolumba257@gmail.com>
2026-04-20 11:44:13 -0400

    initial commit for String Methods

---
+++ src/StringMethods/Main.java
@@ -0,0 +1,20 @@
+package StringMethods;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        String name = "Bro";
+
+        // charAt() - gets character first character (index 0)
+        char result = name.charAt(0);
+        System.out.println(result);
+
+        // equals() - boolean that checks if String input is equal to another
+        boolean result1 = name.equals("Bro");
+        System.out.println(result1);
+
+
+
+    }
+}
