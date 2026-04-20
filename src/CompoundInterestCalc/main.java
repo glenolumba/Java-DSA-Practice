4cacd9612bd0f84083236c3d38d4cd0e3ac17983
Glen Olumba <glenolumba257@gmail.com>
2026-04-20 09:15:47 -0400

    added prompts for user input and displayed final total

--- src/CompoundInterestCalc/main.java
+++ src/CompoundInterestCalc/main.java
@@ -10,10 +10,27 @@ public class main {
 
         Scanner scanner = new Scanner(System.in);
 
+        double principal;
+        double rate;
+        int compounded;
+        int years;
+        double amount;
 
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
 
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble()/100;
 
+        System.out.print("Enter the times it will be compounded in 1 year: ");
+        compounded = scanner.nextInt();
 
+        System.out.print("Enter the amount of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate/compounded, compounded * years);
+
+        System.out.printf("The amount after %d year/s is $%.02f", years, amount);
 
 
 
