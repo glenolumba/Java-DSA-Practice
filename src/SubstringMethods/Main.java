508ce9a94fddd227254b24d07d28b6628044fa7f
Glen Olumba <glenolumba257@gmail.com>
2026-04-20 12:10:34 -0400

    initial commit for substring methods

---
+++ src/SubstringMethods/Main.java
@@ -0,0 +1,12 @@
+package SubstringMethods;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        String email = "mail321@gmail.com";
+        String username = email.substring(0,7);
+        System.out.println(username);
+
+    }
+}
